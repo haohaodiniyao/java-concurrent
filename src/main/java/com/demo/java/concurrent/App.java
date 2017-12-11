f762a87c5047dd2ed30e5cd27e94a9ede156cdf9
yaokai <yaokaiabc@gmail.com>
2017-12-11 18:52:09 +0800

    update

---
+++ src/main/java/com/demo/java/concurrent/App.java
@@ -0,0 +1,13 @@
+package com.demo.java.concurrent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
