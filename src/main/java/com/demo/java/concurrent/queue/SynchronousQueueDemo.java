d71831a7b69cc7a03a552b4365078edeba010ed4
yaokai <yaokaiabc@gmail.com>
2017-12-16 14:43:48 +0800

    hadoop

--- src/main/java/com/demo/java/concurrent/queue/SynchronousQueueDemo.java
+++ src/main/java/com/demo/java/concurrent/queue/SynchronousQueueDemo.java
@@ -1,6 +1,5 @@
 package com.demo.java.concurrent.queue;
 
-import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadLocalRandom;
@@ -15,6 +14,7 @@ import java.util.concurrent.TimeUnit;
 public class SynchronousQueueDemo {
 
 	public static void main(String[] args) {
+		BlockingDeque
 		final SynchronousQueue<String> queue = new SynchronousQueue<String>();
 		new ScheduledThreadPoolExecutor(10).scheduleWithFixedDelay(new Runnable(){
   
